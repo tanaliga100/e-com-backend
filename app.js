fe95473a4b0abcb861fd8669665167d08f28fd4e
tanaliga100 <jordantanaliga@gmail.com>
2023-01-16 23:35:41 +0800

    add-connection-to-Db

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // IMPORT PACKAGE
 const express = require("express");
-
+const connectDB = require("./db/connect");
 // PACKAGE INSTANCE
 const app = express();
-
+require("dotenv").config();
 // TOP MIDDLEWARES
 
 // BOTTOM MIDDLEWARES
@@ -12,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
       console.log(`Server started at ${port} and connected to DB !!`);
     });
--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
   return mongoose.connect(url);
 };
 
