a03a879b3d98164476c31a553876af6cfcc44961
tanaliga100 <jordantanaliga@gmail.com>
2023-01-26 04:32:35 +0800

    add-OrderSchema

---
+++ models/Order.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const SinglCartSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [SinglCartSchema],
+    status: {
+      type: String,
+      enums: ["pending", "failed", "paid", "delivered"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Order", OrderSchema);
